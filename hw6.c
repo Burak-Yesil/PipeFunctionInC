2062b281b03fef59e6d7606b23b04375bd2c705a
Burak Yesil <88214965+Burak-Yesil@users.noreply.github.com>
2022-04-24 22:16:20 -0400

    Update hw6.c

--- hw6.c
+++ hw6.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 
 int main(int argc, char **argv)
 {
@@ -11,7 +13,6 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    chdir(argv[1]); // Changing directory to user specified location from argv
     int fd1[2];
     int fd2[2];
 
@@ -40,13 +41,20 @@ int main(int argc, char **argv)
         close(fd2[1]);
         close(fd1[0]);
         dup2(fd1[1], STDOUT_FILENO);
-        close(fd1[1]);
-
         if (execlp("ls", "ls", "-l", argv[1], NULL) == -1)
         {
             perror("Error: Exec Failed");
         }
+        exit(0);
     }
+
+    int status;
+    waitpid(pid1, &status, 0);
+    if (WEXITSTATUS(status) == 1)
+    {
+        return EXIT_FAILURE;
+    }
+    close(fd1[1]);
 
     int pid2;
     if ((pid2 = fork()) < 0)
@@ -57,38 +65,29 @@ int main(int argc, char **argv)
 
     if (pid2 == 0)
     {
-        close(fd1[1]);
-        dup2(fd1[0], STDIN_FILENO);
-        close(fd1[0]);
         close(fd2[0]);
+        dup2(fd1[0], STDIN_FILENO);
         dup2(fd2[1], STDOUT_FILENO);
-        close(fd2[1]);
+
         if (execlp("grep", "grep", "^d", NULL) == -1)
         {
             perror("Error: Exec failed");
         }
+        exit(0);
     }
 
+    int status2;
+    waitpid(pid2, &status2, 0);
+    if (WEXITSTATUS(status2) == 1)
+    {
+        return EXIT_FAILURE;
+    }
     close(fd1[0]);
-    close(fd1[1]);
-    close(fd2[0]);
     close(fd2[1]);
-
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
-
-    // Working in the main parent process
+    dup2(ds2[0], STDIN_FILENO);
+    int temp = 0;
+    int count = 0; // since there will always be at least 1 \n
     char buf[2048];
-    int lineCount = 0;
-    int temp;
-
-    close(fd1[0]);
-    close(fd1[1]);
-    close(fd2[0]);
-    close(fd2[1]);
-
-    dup2(fd2[0], STDIN_FILENO);
-
     char args[2048];
     while (temp = read(fd2[0], buf, sizeof(buf)) > 0)
     {
@@ -98,10 +97,9 @@ int main(int argc, char **argv)
     char *token = strtok(args, "\n");
     while (token != NULL)
     {
-        lineCount++;
+        count++;
         token = strtok(NULL, "\n");
     }
-    printf("Total amount of subdirectories is: %d\n", lineCount);
-
+    printf("Total amount of subdirectories is: %d\n", count);
     return 0;
 }
